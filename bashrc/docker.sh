b0b356b57669e91dd72d553666d47de6c7579fae
Mouchi Motte <mouchi@mouchi.fr>
2018-10-22 13:28:14 +0200

    add aliases for docker and docker-compose

---
+++ bashrc/docker.sh
@@ -0,0 +1,34 @@
+##
+# DOCKER
+##
+
+alias d="docker"
+alias dps="d ps -a"
+
+# Purge all unused volumes / images and make system prune at the end
+function dpurge ()
+{
+    if [ "$(docker volume ls -q)" != "" ]; then
+        docker volume rm $(docker volume ls -q)
+    fi
+
+    if [ "$(docker image ls -q)" != "" ]; then
+        docker image rm $(docker image ls -q)
+    fi
+    echo y | docker system prune
+}
+
+##
+# DOCKER COMPOSE
+##
+
+alias dc="docker-compose"
+alias dcps="dc ps"
+alias dcup="dc up -d"
+alias dcdown="dc down"
+alias dcexec="docker-compose exec"
+
+function dcbash ()
+{
+    dcexec "$1" bash
+}
