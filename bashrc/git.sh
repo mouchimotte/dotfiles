ccd5744393ff31e7e9d11782c205c779066b07c5
Mouchi Motte <mouchi@mouchi.fr>
2018-12-20 15:15:54 +0100

    update function gprune

--- bashrc/git.sh
+++ bashrc/git.sh
@@ -6,5 +6,12 @@
 # source: https://gist.github.com/maldonado/2fa63770fcd54b92810d
 function gprune ()
 {
-    git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d
+    git checkout master &&
+        git pull --rebase &&
+        git fetch --prune &&
+        git branch -r |
+            awk '{print $1}' |
+            egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) |
+            awk '{print $1}' |
+            xargs git branch -d
 }
