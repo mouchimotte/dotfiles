d7161eeaf388a58da71e735547ec2bd0474a2bff
Mouchi Motte <mouchi@mouchi.fr>
2018-12-20 15:04:30 +0100

    add alias git_prune: remove all branch that do not have remote tracking branch anymore

--- bashrc/helper-alias.sh
+++ bashrc/helper-alias.sh
@@ -11,3 +11,8 @@ alias e='emacs'
 
 # SSH alias
 alias fingerprint='find /etc/ssh -name "*.pub" -exec ssh-keygen -lf {} \; -exec ssh-keygen -E md5 -lf {} \; | tr " " "\t"'
+
+# Git alias
+# Remove git branches that do not have a rmote tracking branch anymore
+# source: https://gist.github.com/maldonado/2fa63770fcd54b92810d
+alias git_prune="git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d"
