67f2b40e59b586072151f21b831248e83bc048a0
Guillaume Motte <guillaume.motte@lumapps.com>
2023-06-29 09:26:16 +0200

    add aliases

--- bashrc/1-helper-functions.sh
+++ bashrc/1-helper-functions.sh
@@ -1,10 +1,3 @@
-COLOR_BG_RED='\033[0;41m'
-COLOR_BG_GREEN='\033[0;42m'
-COLOR_RED='\033[0;31m'
-COLOR_GREEN='\033[0;32m'
-COLOR_YELLOW='\033[0;33m'
-COLOR_NO='\033[00m'
-
 ko () {
     echo -e "${COLOR_BG_RED}KO${COLOR_NO} $@"
     return 1
@@ -26,6 +19,10 @@ title () {
     echo ""
     echo -e "${COLOR_YELLOW}---------- $1${COLOR_NO}"
 }
+
+echo_red () { echo -e "${COLOR_RED}$1${COLOR_NO}"; }
+echo_green () { echo -e "${COLOR_GREEN}$1${COLOR_NO}"; }
+echo_yellow () { echo -e "${COLOR_YELLOW}$1${COLOR_NO}"; }
 
 parse_git_branch()
 {
--- bashrc/helper-alias.sh
+++ bashrc/helper-alias.sh
@@ -11,3 +11,25 @@ alias e='emacs'
 
 # SSH alias
 alias fingerprint='find /etc/ssh -name "*.pub" -exec ssh-keygen -lf {} \; -exec ssh-keygen -E md5 -lf {} \; | tr " " "\t"'
+
+# Helpers
+function len ()
+{
+    python -c "import sys; strings = sys.argv[1:]; print(\"\n\".join([_s + ': ' + str(len(_s)) for _s in strings]));" $@
+}
+
+function uuid_to_int ()
+{
+    python -c "from uuid import UUID; import sys; ids = sys.argv[1:]; print(\"\n\".join([_id + \": \" + str(UUID(_id).int) for _id in ids]));" $@
+}
+
+function int_to_uuid ()
+{
+    python -c "from uuid import UUID; import sys; ids = sys.argv[1:]; print(\"\n\".join([_id + \": \" + str(UUID(int=int(_id))) for _id in ids]));" $@
+}
+
+function dunno_ascii ()
+{
+    echo "¯\_(ツ)_/¯"
+}
+alias ascii_dunno="dunno_ascii"
