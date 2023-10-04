f4b7b2bc4d545369bf4348e89b69cd49f76338b2
Guillaume Motte <guillaume.motte@lumapps.com>
2023-10-04 16:49:23 +0200

    fix prompt + history pimp

--- bashrc/history.sh
+++ bashrc/history.sh
@@ -6,12 +6,14 @@
 # export HISTTIMEFORMAT="%Y-%m-%d %T "
 
 # Increase the history number of lines (default 1000)
-export HISTSIZE=10000
+export HISTSIZE=-1
 # Increase the history number of lines in the file (default 2000, negative mean no limit)
 export HISTFILESIZE=-1
 
 # Append rather than overwrite the history
 shopt -s histappend
+shopt -s histappend histreedit histverify
+shopt -s no_empty_cmd_completion
 # Append immediately (right after being executed) instead of wait the session is terminated
 #PROMPT_COMMAND='$PROMPT_COMMAND; history -a'
 #PROMPT_COMMAND='history -a'
@@ -24,4 +26,4 @@ shopt -s histappend
 # - erasedups: eliminate duplicates across the whole history
 export HISTCONTROL=ignoreboth:erasedups
 # Add the date in the history file to know when it have been executed last time
-export HISTTIMEFORMAT="%F %T "
+# export HISTTIMEFORMAT="%F %T "
